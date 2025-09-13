cab3b4c61c7cc2b8d8d9fa2bd9f5192c2dc21b0f
H4VIC <frederickgoins7@gmail.com>
2025-09-13 17:15:43 -0500

    HackUTD-Added In course Card or something idk

--- src/app/components/coursecard.js
+++ src/app/components/coursecard.js
@@ -9,10 +9,27 @@ const data = [
   }
 ];
 
-const CourseCard = () => {
+const CourseCard = (props) => {
   return (
-    <div>
+    <div className="container mx-auto mt-5 p-6 bg-white shadow rounded">
       
+      <h1 className="text-3xl font-bold text-blue-600 mb-3">
+        {props.CourseCard}
+      </h1>
+
+      <h2 className="text-xl font-semibold text-gray-800 mb-3">
+        {props.title}
+      </h2>
+
+      <p className="text-gray-500 mb-4">
+        {props.description}
+      </p>
+
+      <div className="inline-black">
+        <span className="bg-green-100 text-green-700 px-3 py-1 rounded-full text-sm font-median">
+          2 Credit Hours
+        </span>
+      </div>
     </div>
   );
 };
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Link from "next/link";
 import { User, Settings } from "lucide-react";
+import { Angry } from 'lucide-react';
+import { BriefcaseMedical } from 'lucide-react';
+import { Frown } from 'lucide-react';
 
 function Navbar() {
     return (
-        <div>
-            
+         <nav className = "flex justify-between items-center py-4 px-40 bg-gray-100 border-b border-gray-300 text-black">      
+        <Link href="/settings" className="group relative">
+            <Settings size={50} className="cursor-pointer"/>
+        </Link>
+        <div className="font-bold text-2xl">
+            <span className="cursor-pointer text-gray-900">NotDevGPA</span>
         </div>
+        <Link href="/profile" className="group relative">
+            <User size={50} className="cursor-pointer"/>
+        </Link>
+        </nav>
     );
 }
 
